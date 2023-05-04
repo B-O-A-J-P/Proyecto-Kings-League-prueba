137bc7cd629d861721e3f4908e0cb3999821d735
Ander Mora <ander.mora@ikasle.egibide.org>
2023-05-04 13:30:24 +0200

    13:30 04/05/2023 cambios

--- BaseDeDatos/kings_league_triggers.sql
+++ BaseDeDatos/kings_league_triggers.sql
@@ -1,4 +1,3 @@
-
 drop trigger max_jugadores;
 drop trigger min_jugadores;
 drop trigger min_equipos;
@@ -47,17 +46,20 @@ create or replace trigger max_jugadores_draft
 before insert on contratos_equipo_jugador
 for each row
 declare
-    codEquipo contratos_equipo_jugador.cod_equipo%type := :new.cod_equipo;
     numeroContratos number;
+    estaEnDraft number;
 begin
     select count(*) into numeroContratos
     from contratos_equipo_jugador cj, draft d
-    where cod_equipo = codEquipo 
+    where cod_equipo = :new.cod_equipo 
     and cj.cod_jugador = d.cod_jugador 
-    and d.cod_temporada = (select max(cod_temporada) from draft)
+    and d.cod_temporada = (select max(cod_temporada) from temporadas)
     and (fecha_fin > sysdate or fecha_fin is null);
     
-    if numeroContratos > 8
+    select count(*) into estaEnDraft from draft where cod_jugador = :new.cod_jugador
+    and cod_temporada = (select max(cod_temporada) from temporadas);
+    
+    if numeroContratos >= 8 and estaEnDraft > 0
     then 
         raise_application_error(-20001, 'El equipo ya tiene 8 jugadores');
     end if;
@@ -66,20 +68,20 @@ end max_jugadores;
 
 /
 
+
 create or replace trigger max_jugadores_wild_card
 before insert on contratos_equipo_jugador
 for each row
 declare
-    codEquipo contratos_equipo_jugador.cod_equipo%type := :new.cod_equipo;
     numeroContratos number;
 begin
     select count(*) into numeroContratos
     from contratos_equipo_jugador cj
-    where cod_equipo = codEquipo 
-    and cj.cod_jugador not in (select cod_jugador from draft where cod_temporada = (select max(cod_temporada) from draft))
+    where cod_equipo = :new.cod_equipo 
+    and cj.cod_jugador not in (select cod_jugador from draft where cod_temporada = (select max(cod_temporada) from temporadas))
     and (fecha_fin > sysdate or fecha_fin is null);
     
-    if numeroContratos > 2
+    if numeroContratos >= 2
     then 
         raise_application_error(-20001, 'El equipo ya tiene 2 jugadores fuera del draft');
     end if;
@@ -87,11 +89,128 @@ begin
 end max_jugadores;
 /
 create or replace trigger min_clausula_inicial
-	before insert or update on contratos_equipo_jugador
-	for each row
-		begin
-			if :new.clausula < 1000000 then
-				raise_application_error(-20004, 'La clausula minima inicial es de 1.000.000');
-			end if;
+    before insert or update on contratos_equipo_jugador
+    for each row
+        begin
+            if :new.clausula < 1000000 then
+                raise_application_error(-20004, 'La clausula minima inicial es de 1.000.000');
+            end if;
 end min_clausula_inicial;
 
+-- controlar que solo haya un entrenador
+
+create or replace trigger control_miembros
+before insert or update on contratos_equipo_miembro
+for each row
+declare
+    numberoDeMiembros number;
+begin
+    select count(*) into numberoDeMiembros
+    from contratos_equipo_miembro
+    where cod_equipo = :new.cod_equipo
+    and funcion = :new.funcion
+    and (fecha_salida > sysdate or fecha_salida is null);
+    
+    if numberoDeMiembros >= 1
+    then
+        raise_application_error(-20001, 'No puede haber más de un miembro con la misma función');
+    end if;
+    
+end control_miembros;
+
+CREATE OR REPLACE TRIGGER max_presupuesto_equipo
+before INSERT OR UPDATE on contratos_equipo_jugador
+for each row
+declare
+    v_clausula number;
+    v_salario number;
+    v_pres_max number;
+  BEGIN
+    SELECT presupuesto INTO v_pres_max
+    FROM equipos
+    WHERE cod_equipo = :new.cod_equipo;
+         
+    SELECT SUM(salario), SUM(clausula) INTO v_salario, v_clausula
+    FROM contratos_equipo_jugador
+    WHERE cod_equipo = :new.cod_equipo
+    AND (fecha_fin > sysdate OR fecha_fin IS NULL);
+    
+    if (v_salario + v_clausula) > 200000000
+    then
+    raise_application_error(-20001, 'no puede sobrepasarse el presupuesto
+    establecido');
+    end if;
+
+END max_presupuesto_equipo;
+
+CREATE OR REPLACE TRIGGER check_contrato_jugador_split
+BEFORE INSERT OR UPDATE OF cod_jugador, fecha_inicio, fecha_fin
+ON contratos_equipo_jugador
+FOR EACH ROW
+DECLARE
+	v_count INTEGER;
+BEGIN
+	SELECT COUNT(*) INTO v_count
+	FROM contratos_equipo_jugador
+	WHERE cod_jugador = :NEW.cod_jugador
+	AND (fecha_fin > sysdate
+
+	or fecha_fin is null);
+    
+	IF v_count > 0 THEN
+    	RAISE_APPLICATION_ERROR(-20001, 'Este jugador ya tiene un contrato activo');
+	END IF;
+END;
+
+
+
+create or replace trigger bloquear_splits
+before insert or update on splits
+for each row
+declare
+begin
+    raise_application_error(-20001, 'No se pueden modifcar splits antes o después de generar el calendario');
+end;
+/
+
+alter trigger bloquear_splits disable;
+
+
+create or replace trigger bloquear_jornadas
+before insert or update on splits
+for each row
+declare
+begin
+    raise_application_error(-20001, 'No se pueden modifcar jornadas antes o después de generar el calendario');
+end;
+
+alter trigger bloquear_jornadas disable;
+
+create or replace trigger bloquear_partidos
+before insert or update on splits
+for each row
+declare
+begin
+    raise_application_error(-20001, 'No se pueden modifcar partidos antes o después de generar el calendario');
+end;
+
+alter trigger bloquear_partidos disable;
+
+-- Los triggers están desactivo por defecto. Se activaran cuando se genere el calendario mediante el procedimiento
+-- si se desea modificar durante lo jornada, solo se podra desactivar utilizando un procedimiento del paquete calendario
+
+
+
+CREATE OR REPLACE TRIGGER equipo_duplicado_tr
+BEFORE INSERT or update ON equipos
+FOR EACH ROW
+    DECLARE v_num_equi INT;
+BEGIN
+
+    SELECT COUNT(*) into v_num_equi 
+    FROM equipos
+     WHERE nombre = :NEW.nombre;
+    IF v_num_equi > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'ERROR, EQUIPO YA EXISTENTE');
+    END IF;
+END equipo_duplicado_tr;
---
+++ BaseDeDatos/tablas_usuarios.sql
@@ -0,0 +1,29 @@
+drop table cuentas cascade constraints;
+drop table permisos cascade constraints;
+
+
+CREATE TABLE permisos(
+    cod_perfil NUMBER(5, 0) GENERATED ALWAYS AS IDENTITY INCREMENT BY 1 START WITH 0 MINVALUE 0 NOCYCLE NOT NULL ENABLE,
+    insertar varchar2(1),
+    eliminar varchar2(1),
+    actualizar varchar2(1),
+    ver_vista varchar2(1),
+    ver_global varchar2(1),
+    lanzar_procedimientos varchar2(1),
+    CONSTRAINTS per_ins_ck CHECK(insertar in ('t', 'f')),
+    CONSTRAINTS per_act_ck CHECK(actualizar in ('t', 'f')),
+    CONSTRAINTS per_vis_ck CHECK(ver_vista in ('t', 'f')),
+    CONSTRAINTS per_glo_ck CHECK(ver_global in ('t', 'f')),
+    CONSTRAINTS per_pro_ck CHECK(lanzar_procedimientos in ('t', 'f')),
+    CONSTRAINT per_cod_pk PRIMARY KEY (cod_perfil)
+);
+
+CREATE TABLE CUENTAS (
+    cod_cuenta number(6, 0) GENERATED ALWAYS AS IDENTITY INCREMENT BY 1 START WITH 0 MINVALUE 0 NOCYCLE NOT NULL ENABLE,
+    cod_perfil NUMBER(5, 0),
+    usuario varchar2(50),
+    contraseña varchar2(50),
+    email varchar2(60),
+    CONSTRAINT cue_cue_pk PRIMARY KEY(cod_cuenta),
+    CONSTRAINT cod_perfil FOREIGN KEY(cod_perfil) REFERENCES permisos
+);
