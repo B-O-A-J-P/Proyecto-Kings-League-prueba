51b600714a692a4af56436c3fc547364e7aae226
jorge <jorge.egea@ikasle.egibide.org>
2023-05-10 10:24:51 +0200

    xml

--- Programacion/kings_league/src/main/java/com/boajp/vista/calendario.java
+++ Programacion/kings_league/src/main/java/com/boajp/vista/calendario.java
@@ -15,7 +15,7 @@ import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 
-
+import java.io.File;
 import java.io.IOException;
 
 
@@ -23,15 +23,40 @@ public class calendario {
     private JPanel pCalendario;
 
     public calendario(){
+        try {
 
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            Document document = builder.parse(new File("src/main/java/com/boajp/xml/Calendario_kings_league.xml"));
+
+            NodeList partidoList = document.getElementsByTagName("partido");
+
+            for (int i = 0; i < partidoList.getLength(); i++) {
+                Element partidoElement = (Element) partidoList.item(i);
+
+                NodeList equipoList = partidoElement.getElementsByTagName("equipo");
+
+                for (int j = 0; j < equipoList.getLength(); j++) {
+                    Element equipoElement = (Element) equipoList.item(j);
+
+                    String id = equipoElement.getAttribute("id");
+                    String nombre = equipoElement.getElementsByTagName("nombre").item(0).getTextContent();
+                    String logo = equipoElement.getElementsByTagName("logo").item(0).getTextContent();
+
+                    System.out.println("ID: " + id);
+                    System.out.println("Nombre: " + nombre);
+                    System.out.println("Logo: " + logo);
+                    System.out.println();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        JFrame frame = new JFrame("calendario");
-        frame.setContentPane(new calendario().pCalendario);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        new calendario();
     }
 }
