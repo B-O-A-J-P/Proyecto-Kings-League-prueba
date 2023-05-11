955bf6bc10d71a33638b0c210ee6bb6ddd118664
Ander Mora <ander.mora@ikasle.egibide.org>
2023-05-11 14:04:46 +0200

    cambios inserts

--- BaseDeDatos/3-Dummy-inserts-y-pruebas/inserts.sql
+++ BaseDeDatos/3-Dummy-inserts-y-pruebas/inserts.sql
@@ -1,9 +1,4 @@
-INSERT INTO temporadas (ano, fecha_inicio_inscripcion, fecha_fin_inscripcion) VALUES (2022, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('30/06/2022', 'DD/MM/YYYY'));
-
-INSERT INTO splits (cod_temporada, nombre, fecha_inicio, fecha_fin) VALUES (0, 'Split 1', TO_DATE('01/07/2021', 'DD/MM/YYYY'), TO_DATE('31/07/2021', 'DD/MM/YYYY'));
-INSERT INTO splits (cod_temporada, nombre, fecha_inicio, fecha_fin) VALUES (0, 'Split 2', TO_DATE('01/07/2021', 'DD/MM/YYYY'), TO_DATE('31/07/2021', 'DD/MM/YYYY'));
-
-INSERT INTO jornadas (cod_split, numero, fecha, ubicacion) VALUES (0, 1, TO_DATE('01/07/2021', 'DD/MM/YYYY'), 'Estadio A');
+INSERT INTO temporadas (ano, fecha_inicio_inscripcion, fecha_fin_inscripcion) VALUES (2023, TO_DATE('01/01/2023', 'DD/MM/YYYY'), TO_DATE('30/06/2023', 'DD/MM/YYYY'));
 
 INSERT INTO equipos (nombre, logo, presupuesto) VALUES ('Equipo A', EMPTY_BLOB(), 1000000);
 INSERT INTO equipos (nombre, logo, presupuesto) VALUES ('Equipo B', EMPTY_BLOB(), 1000000);
@@ -18,15 +13,20 @@ INSERT INTO equipos (nombre, logo, presupuesto) VALUES ('Equipo J', EMPTY_BLOB()
 INSERT INTO equipos (nombre, logo, presupuesto) VALUES ('Equipo K', EMPTY_BLOB(), 850000);
 INSERT INTO equipos (nombre, logo, presupuesto) VALUES ('Equipo L', EMPTY_BLOB(), 1050000);
 
-INSERT INTO partidos (cod_jornada, cod_equipo1, cod_equipo2, hora, fase) VALUES (0, 0, 1, TO_TIMESTAMP('01/07/2021 10:00:00', 'DD/MM/YYYY HH24:MI:SS'), 'r');
 
-INSERT INTO clasificaciones (cod_split, cod_equipo, posicion) VALUES (0, 0, 1);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 0);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 1);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 2);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 3);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 4);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 5);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 6);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 7);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 8);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 9);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 10);
+INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 11);
 
-INSERT INTO clasificaciones (cod_split, cod_equipo, posicion) VALUES (0, 1, 1);
-
-INSERT INTO informacion_partidos (cod_partido, cod_equipo, numero_goles, resultado) VALUES (1, 0, 2, 'v');
-
-INSERT INTO informacion_partidos (cod_partido, cod_equipo, numero_goles, resultado) VALUES (1, 1, 0, 'd');
 
 INSERT INTO agendas (email, telefono) VALUES ('example@example.com', '123456789');
 INSERT INTO agendas (email, telefono) VALUES ('john@example.com', '987654321');
@@ -51,6 +51,14 @@ INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('
 INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('89012351P', 'Jugador', 'Once', 'Derecho', 200, 7);
 INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('99012351P', 'Jugador', 'Doce', 'Derecho', 200, 7);
 
+INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('00112233A', 'Jugador', 'Trece', 'Izquierdo', 178, 0);
+INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('11223344B', 'Jugador', 'Catorce', 'Derecho', 183, 1);
+INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('22334455C', 'Jugador', 'Quince', 'Izquierdo', 175, 2);
+INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('33445566D', 'Jugador', 'Dieciséis', 'Derecho', 192, 3);
+INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('44556677E', 'Jugador', 'Diecisiete', 'Izquierdo', 172, 4);
+INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('55667788F', 'Jugador', 'Dieciocho', 'Derecho', 197, 5);
+INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('66778899G', 'Jugador', 'Diecinueve', 'Izquierdo', 183, 6);
+INSERT INTO jugadores (dni, nombre, apellido, pie, altura, cod_agenda) VALUES ('77889900H', 'Jugador', 'Veinte', 'Derecho', 202, 7);
 
 
 INSERT INTO registros_jugadores (cod_temporada, cod_jugador) VALUES (0, 0);
@@ -67,9 +75,6 @@ INSERT INTO registros_jugadores (cod_temporada, cod_jugador) VALUES (0, 10);
 INSERT INTO registros_jugadores (cod_temporada, cod_jugador) VALUES (0, 11);
 
 
-
-
-
 INSERT INTO draft (cod_temporada, cod_jugador, posicion) VALUES (0, 0, 1);
 INSERT INTO draft (cod_temporada, cod_jugador, posicion) VALUES (0, 1, 2);
 INSERT INTO draft (cod_temporada, cod_jugador, posicion) VALUES (0, 2, 3);
@@ -78,23 +83,21 @@ INSERT INTO draft (cod_temporada, cod_jugador, posicion) VALUES (0, 4, 5);
 INSERT INTO draft (cod_temporada, cod_jugador, posicion) VALUES (0, 5, 6);
 INSERT INTO draft (cod_temporada, cod_jugador, posicion) VALUES (0, 6, 7);
 INSERT INTO draft (cod_temporada, cod_jugador, posicion) VALUES (0, 7, 8);
-INSERT INTO draft (cod_temporada, cod_jugador, posicion) VALUES (0, 8, 9);
 
 
 
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 0, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 1, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 2, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 3, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 4, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 5, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 6, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 7, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 8, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 9, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 10, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 11, 10500000, 10000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
-
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 0, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 1, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 2, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 3, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 4, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 5, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 6, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 7, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 8, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 9, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 10, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
+INSERT INTO contratos_equipo_jugador (cod_equipo, cod_jugador, salario, clausula, fecha_inicio, fecha_fin) VALUES (0, 11, 10500000, 1000000, TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2023', 'DD/MM/YYYY'));
 
 
 INSERT INTO miembros (dni, nombre, apellido, cod_agenda)
@@ -110,17 +113,31 @@ INSERT INTO contratos_equipo_miembro (cod_equipo, cod_miembro, funcion, fecha_en
 VALUES (1, 1, 'p', TO_DATE('01/01/2022', 'DD/MM/YYYY'), TO_DATE('31/12/2022', 'DD/MM/YYYY'));
 
 
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 0);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 1);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 2);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 3);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 4);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 5);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 6);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 7);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 8);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 9);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 10);
-INSERT INTO registros_equipos (cod_temporada, cod_equipo) VALUES (0, 11);
+
+
+
+
+INSERT INTO splits (cod_temporada, nombre, fecha_inicio, fecha_fin) VALUES (0, 'Split 1', TO_DATE('01/07/2023', 'DD/MM/YYYY'), TO_DATE('31/07/2023', 'DD/MM/YYYY'));
+INSERT INTO splits (cod_temporada, nombre, fecha_inicio, fecha_fin) VALUES (0, 'Split 2', TO_DATE('01/07/2023', 'DD/MM/YYYY'), TO_DATE('31/07/2023', 'DD/MM/YYYY'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
