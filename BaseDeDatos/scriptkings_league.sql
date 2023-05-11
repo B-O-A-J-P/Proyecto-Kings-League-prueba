79dee28f85bb106457a83cbed563f92e43d0490c
OscarCavadas <126677233+OscarCavadas@users.noreply.github.com>
2023-05-11 12:25:25 +0200

    Update scriptkings_league.sql

--- BaseDeDatos/scriptkings_league.sql
+++ BaseDeDatos/scriptkings_league.sql
@@ -1,59 +1,59 @@
 --guarda el contenido del proyecto en la carpeta documentos
 --ejecutar tabla 1
 --WINDOWS
-@/$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\1-Scripts_tablas_y_otros_objetos\tablas.sql
+@"$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\1-Scripts_tablas_y_otros_objetos\tablas.sql"
 
 --ejecutar tabla 2
-@/$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\1-Scripts_tablas_y_otros_objetos\tablas_usuarios.sql
+@"$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\1-Scripts_tablas_y_otros_objetos\tablas_usuarios.sql"
 
 --ejecutar vistas
-@/$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\1-Scripts_tablas_y_otros_objetos\vistas.sql
+@"$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\1-Scripts_tablas_y_otros_objetos\vistas.sql"
 
 --ejecutar vistas_miembros
-@/$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\1-Scripts_tablas_y_otros_objetos\vistas_miembros.sql
+@"$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\1-Scripts_tablas_y_otros_objetos\vistas_miembros.sql"
 
 
 --ejecutar triggers
-@/$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\2-Scripts-triggers\triggers.sql
+@"$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\2-Scripts-triggers\triggers.sql"
 
 --ejecutar triggers
-@/$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\2-Scripts-triggers\triggers_bloquear.sql
+@"$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\2-Scripts-triggers\triggers_bloquear.sql"
 
 --3-Dummy inserts y pruebas
-@/$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\3-Dummy-inserts-y-pruebas\inserts.sql
+@"$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\3-Dummy-inserts-y-pruebas\inserts.sql"
 
 --4-Procedimientos-almacenados
-@/$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\4-Procedimientos-almacenados\procedimiento_almacenado_objetos.sql
+@"$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\4-Procedimientos-almacenados\procedimiento_almacenado_objetos.sql"
 
 --5-Paquete
-@/$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\5-Paquete\cabecera.sql
-@/$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\5-Paquete\cuerpo.sql
+@"$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\5-Paquete\cabecera.sql"
+@"$HOME\Documents\proyecto_kingsleague\Proyecto-Kings-League\BaseDeDatos\5-Paquete\cuerpo.sql"
 
 --MAC
 --ejecutar tabla 1
-@/$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/1-Scripts_tablas_y_otros_objetos/tablas.sql
+@"$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/1-Scripts_tablas_y_otros_objetos/tablas.sql"
 
 --ejecutar tabla 2
-@/$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/1-Scripts_tablas_y_otros_objetos/tablas_usuarios.sql
+@"$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/1-Scripts_tablas_y_otros_objetos/tablas_usuarios.sql"
 
 --ejecutar vistas
-@/$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/1-Scripts_tablas_y_otros_objetos/vistas.sql
+@"$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/1-Scripts_tablas_y_otros_objetos/vistas.sql"
 
 --ejecutar vistas_miembros
-@/$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/1-Scripts_tablas_y_otros_objetos/vistas_miembros.sql
+@"$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/1-Scripts_tablas_y_otros_objetos/vistas_miembros.sql"
 
 --ejecutar triggers
-@/$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/2-Scripts-triggers/triggers.sql
+@"$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/2-Scripts-triggers/triggers.sql"
 
 --ejecutar triggers
-@/$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/2-Scripts-triggers/triggers_bloquear.sql
+@"$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/2-Scripts-triggers/triggers_bloquear.sql"
 
 --3-Dummy inserts y pruebas
-@/$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/3-Dummy-inserts-y-pruebas/inserts.sql
+@"$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/3-Dummy-inserts-y-pruebas/inserts.sql"
 
 --4-Procedimientos-almacenados
-@/$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/4-Procedimientos-almacenados/procedimiento_almacenado_objetos.sql
+@"$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/4-Procedimientos-almacenados/procedimiento_almacenado_objetos.sql"
 
 --5-Paquete
-@/$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/5-Paquete/cabecera.sql
-@/$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/5-Paquete/cuerpo.sql
+@"$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/5-Paquete/cabecera.sql"
+@"$HOME/Documents/proyecto_kingsleague/Proyecto-Kings-League/BaseDeDatos/5-Paquete/cuerpo.sql"
