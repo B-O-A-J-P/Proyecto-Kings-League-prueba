e43acbae6a682e0e0e7f28952a280c7279d31e9c
Ander Mora <ander.mora@ikasle.egibide.org>
2023-05-17 09:29:47 +0200

    comentarios

--- Programacion/kings_league/src/main/java/com/boajp/vista/Administrador/BarraLateral.java
+++ Programacion/kings_league/src/main/java/com/boajp/vista/Administrador/BarraLateral.java
@@ -11,4 +11,12 @@ public class BarraLateral {
     private JButton verVistaButton;
     private JButton verGlobalButton;
     private JButton generarButton;
+
+    //TODO Esta es la barra que aparece en la izquierda cuando eres admin, despues de clickar, en el medio de la ventana
+    //TODO aparece PanelAdmin con las 3 opciones de equipos,jornada,o jugadores
+    //TODO el boton eliminar de todas las vistas se refiere a "limpiar" la zona de los text fields por haber escrito mal
+    //TODO la idea es que si le das a insertar te deje modificar los text fields y eliminarlos antes de subirlos
+    //TODO en los demas metodos, eliminar, actualizar, la idea es que cuando escriba el primer dato
+    //TODO (equipos->nombre,jugadores->DNI,jornada->numero) se rellene automaticamente los demas campos, para ver que
+    //TODO se desea modificar o que se desea eliminar
 }
