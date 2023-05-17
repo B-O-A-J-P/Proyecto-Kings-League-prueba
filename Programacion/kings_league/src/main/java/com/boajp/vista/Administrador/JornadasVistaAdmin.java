9e6171604a1d18decb841a086ae10961b60e71df
Ander Mora <ander.mora@ikasle.egibide.org>
2023-05-17 09:13:58 +0200

    cambios 9:13

---
+++ Programacion/kings_league/src/main/java/com/boajp/vista/Administrador/JornadasVistaAdmin.java
@@ -0,0 +1,11 @@
+package com.boajp.vista.Administrador;
+
+import javax.swing.*;
+
+public class JornadasVistaAdmin {
+    private JPanel panel1;
+    private JButton eliminarButton;
+    private JTextField tfNumero;
+    private JTextField tfFecha;
+    private JTextField tfUbicacion;
+}
---
+++ Programacion/kings_league/src/main/java/com/boajp/vista/Administrador/JugadoresVistaAdmin.java
@@ -0,0 +1,12 @@
+package com.boajp.vista.Administrador;
+
+import javax.swing.*;
+
+public class JugadoresVistaAdmin {
+    private JTextField tfDni;
+    private JTextField tfNombre;
+    private JTextField tfApellido;
+    private JTextField tfPie;
+    private JTextField tfAltura;
+    private JButton bActualizar;
+}
