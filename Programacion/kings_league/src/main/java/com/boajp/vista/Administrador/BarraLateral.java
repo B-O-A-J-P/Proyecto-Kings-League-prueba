40526c2964a31b70f350c3116d5f388d91d736cf
Ander Mora <ander.mora@ikasle.egibide.org>
2023-05-17 09:13:58 +0200

    cambios 9:13

---
+++ Programacion/kings_league/src/main/java/com/boajp/vista/Administrador/BarraLateral.java
@@ -0,0 +1,14 @@
+package com.boajp.vista.Administrador;
+
+import javax.swing.*;
+
+public class BarraLateral {
+    private JPanel pPrincipal;
+    private JButton perfilButton;
+    private JButton insertarButton;
+    private JButton eliminarButton;
+    private JButton actualizarButton;
+    private JButton verVistaButton;
+    private JButton verGlobalButton;
+    private JButton generarButton;
+}
