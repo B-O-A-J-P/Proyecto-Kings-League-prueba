e3b2b19c1c86cdd4cf45fbf737ef2f626ffe0392
jorge <jorge.egea@ikasle.egibide.org>
2023-05-18 12:57:14 +0200

    fix

--- BaseDeDatos/1-Scripts_tablas_y_otros_objetos/sinonimos.sql
+++ BaseDeDatos/1-Scripts_tablas_y_otros_objetos/sinonimos.sql
@@ -1,7 +1,3 @@
-DROP PUBLIC SYNONYM TS;
-DROP PUBLIC SYNONYM PS;
-DROP PUBLIC SYNONYM JS;
-DROP PUBLIC SYNONYM ES;
 DROP PUBLIC SYNONYM num_jug_equ;
 DROP PUBLIC SYNONYM inf_equ_tem;
 DROP PUBLIC SYNONYM inf_jug_ult_tem;
@@ -11,12 +7,12 @@ DROP PUBLIC SYNONYM ent_equ;
 DROP PUBLIC SYNONYM sta_equ;
 
 --Funciones y procedimientos almacenados
-CREATE PUBLIC SYNONYM num_jug_equ FOR contar_numero_jugadores_por_equipo;
-CREATE PUBLIC SYNONYM inf_equ_tem FOR informe_equipos_temporada;
-CREATE PUBLIC SYNONYM inf_jug_ult_tem FOR informe_jugador_ultima_temporada;
-CREATE PUBLIC SYNONYM get_num_jug_dra FOR get_numero_jugadores_draft;
+CREATE PUBLIC SYNONYM num_jug_equ FOR eqdaw05.contar_numero_jugadores_por_equipo;
+CREATE PUBLIC SYNONYM inf_equ_tem FOR eqdaw05.informe_equipos_temporada;
+CREATE PUBLIC SYNONYM inf_jug_ult_tem FOR eqdaw05.informe_jugador_ultima_temporada;
+CREATE PUBLIC SYNONYM get_num_jug_dra FOR eqdaw05.get_numero_jugadores_draft;
 
 --Vistas
-CREATE PUBLIC SYNONYM pre_equ FOR presidentes_equipos;
-CREATE PUBLIC SYNONYM ent_equ FOR entrenadores_equipos;
-CREATE PUBLIC SYNONYM sta_equ FOR staffs_equipos;
+CREATE PUBLIC SYNONYM pre_equ FOR eqdaw05.presidentes_equipos;
+CREATE PUBLIC SYNONYM ent_equ FOR eqdaw05.entrenadores_equipos;
+CREATE PUBLIC SYNONYM sta_equ FOR eqdaw05.staffs_equipos;
