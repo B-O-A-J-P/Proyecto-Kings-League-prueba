f8acc1c514c2296fce8690b338624272a8497e64
Ander Mora <ander.mora@ikasle.egibide.org>
2023-05-19 08:53:25 +0200

    cambios ander

---
+++ Programacion/kings_league/src/main/java/com/boajp/vista/UsuarioFinal/AgendaVistaCRUD.java
@@ -0,0 +1,11 @@
+package com.boajp.vista.UsuarioFinal;
+
+import javax.swing.*;
+
+public class AgendaVistaCRUD {
+    private JButton actualizarButton;
+    private JButton eliminarButton;
+    private JButton insertarButton;
+    private JTextField tfEmail;
+    private JTextField tfTelefono;
+}
